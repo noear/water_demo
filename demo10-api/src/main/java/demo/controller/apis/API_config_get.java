2a876fd7aef5b7016f14be106e0885b7e70fd25f
noear <noear@live.cn>
2021-01-13 15:41:00 +0800

    demo

--- demo10-api/src/main/java/demo/controller/apis/API_config_get.java
+++ demo10-api/src/main/java/demo/controller/apis/API_config_get.java
@@ -1,11 +1,11 @@
 package demo.controller.apis;
 
+import demo.controller.ApiBase;
 import demo.dso.mapper.WaterMapper;
 import demo.model.ConfigModel;
 import org.noear.solon.annotation.Component;
 import org.noear.solon.annotation.Mapping;
-import org.noear.solon.extend.validation.annotation.Valid;
-import org.noear.water.WaterClient;
+import org.noear.solon.extend.validation.annotation.NotNull;
 import org.noear.weed.annotation.Db;
 
 import java.sql.SQLException;
@@ -13,13 +13,13 @@ import java.sql.SQLException;
 /**
  * @author noear 2021/1/13 created
  */
-@Valid
 @Component(tag = "api")
-public class API_config_get {
+public class API_config_get extends ApiBase {
 
     @Db
     WaterMapper waterMapper;
 
+    @NotNull("user_id")
     @Mapping("config/get")
     public ConfigModel exec(long user_id) throws SQLException {
         return waterMapper.getConfig("water", "water");
