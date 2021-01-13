39a68a6fbf491c053ebc11951b0def22f80efcf2
noear <noear@live.cn>
2021-01-13 16:09:54 +0800

    demo

--- demo10-api/src/main/java/demo/Config.java
+++ demo10-api/src/main/java/demo/Config.java
@@ -1,10 +1,11 @@
 package demo;
 
 import com.zaxxer.hikari.HikariDataSource;
-import org.noear.solon.Solon;
 import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Configuration;
 import org.noear.solon.annotation.Inject;
+import org.noear.water.utils.CacheUtils;
+import org.noear.weed.cache.LocalCache;
 
 import javax.sql.DataSource;
 
@@ -13,8 +14,23 @@ import javax.sql.DataSource;
  */
 @Configuration
 public class Config {
+    /**
+     * Water 数据源
+     * ${water.water} 配置是由从配置服务加载的配置
+     */
     @Bean("db_water")
-    public DataSource water(@Inject("${water.water}") HikariDataSource ds){
+    public DataSource waterDb(@Inject("${water.water}") HikariDataSource ds) {
         return ds;
+    }
+
+    /**
+     * 泛缓存
+     *
+     * CacheUtils.CacheWrap 对：Solon:CacheService 和 Weed3:ICacheServiceEx 做了双重适配
+     */
+    @Bean("cache")
+    public CacheUtils.CacheWrap cache() {
+        //对Weed3:ICacheServiceEx 做包装，以兼容Weed3 与 Solon的双重需求
+        return CacheUtils.wrap(new LocalCache());
     }
 }
