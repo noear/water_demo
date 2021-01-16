f771af6ed3d1e4dddc2b9909a029cc099244866a
noear <noear@live.cn>
2021-01-16 11:10:41 +0800

    demo

--- demo10-api/src/main/java/demo/controller/CmdGateway.java
+++ demo10-api/src/main/java/demo/controller/CmdGateway.java
@@ -1,6 +1,8 @@
 package demo.controller;
 
 import demo.controller.interceptor.AuthInterceptor;
+import org.noear.snack.ONode;
+import org.noear.solon.Utils;
 import org.noear.solon.annotation.Component;
 import org.noear.solon.annotation.Mapping;
 import org.noear.solon.core.handle.Context;
@@ -18,17 +20,29 @@ public class CmdGateway extends GatewayBase {
     }
 
     @Override
+    protected void handlePre(Context c) throws Throwable {
+        String json = c.body();
+        if (Utils.isEmpty(json)) {
+            throw ApiCodes.CODE_14;
+        } else {
+            ONode tmp = ONode.load(json);
+            tmp.forEach((k, v) -> {
+                c.paramMap().put(k, v.getString());
+            });
+
+            if (c.param("method") == null) {
+                throw ApiCodes.CODE_14;
+            }
+        }
+    }
+
+    @Override
     protected boolean allowPathMerging() {
         return false;
     }
 
     @Override
     protected String getPathDo(Context c) {
-        String tmp = c.param("method");
-        if (tmp == null) {
-            return null;
-        } else {
-            return tmp.toUpperCase();
-        }
+        return c.param("method").toUpperCase();
     }
 }
