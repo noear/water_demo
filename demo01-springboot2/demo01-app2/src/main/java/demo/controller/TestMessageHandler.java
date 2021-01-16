ad25c4ba536749f2c44ab3888e0e9fbb7642f8f5
noear <noear@live.cn>
2021-01-16 22:44:07 +0800

    demo

--- demo01-springboot2/demo01-app2/src/main/java/demo/controller/TestMessageHandler.java
+++ demo01-springboot2/demo01-app2/src/main/java/demo/controller/TestMessageHandler.java
@@ -8,7 +8,7 @@ import org.noear.solon.cloud.model.Event;
  * 消息订阅（可以自己发出去，再自己订阅回来；起来拆解性能消耗的作用）
  *
  * */
-@CloudEvent("test.hello")
+@CloudEvent(topic = "test.hello")
 public class TestMessageHandler implements CloudEventHandler {
 
     @Override
