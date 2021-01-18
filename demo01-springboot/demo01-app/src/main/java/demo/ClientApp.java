3ea045244c09faa246d5354feb79121dab46bbcd
noear <noear@live.cn>
2021-01-18 13:59:04 +0800

    demo

--- demo01-springboot/demo01-app/src/main/java/demo/ClientApp.java
+++ demo01-springboot/demo01-app/src/main/java/demo/ClientApp.java
@@ -1,15 +1,11 @@
 package demo;
 
-import org.noear.nami.integration.springboot.EnableNamiClients;
-import org.noear.solon.cloud.integration.springboot.EnableCloudClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author noear 2020/12/28 created
  */
-@EnableNamiClients
-@EnableCloudClients
 @SpringBootApplication
 public class ClientApp {
     public static void main(String[] args) {
--- demo01-springboot/demo01-rpc-service/src/main/java/demo/ServarApp.java
+++ demo01-springboot/demo01-rpc-service/src/main/java/demo/ServarApp.java
@@ -1,13 +1,11 @@
 package demo;
 
-import org.noear.solon.cloud.integration.springboot.EnableCloudClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author noear 2021/1/8 created
  */
-@EnableCloudClients
 @SpringBootApplication
 public class ServarApp {
     public static void main(String[] args) {
