b67aead519b57f6a6f855572ff2b5566456956ed
noear <noear@live.cn>
2021-04-06 16:06:05 +0800

    1.3.20

--- demo02-solon/demo02-app/src/main/java/demo/controller/TestConfigHandler.java
+++ demo02-solon/demo02-app/src/main/java/demo/controller/TestConfigHandler.java
@@ -1,18 +1,23 @@
 package demo.controller;
 
-import org.noear.water.annotation.WaterConfig;
-import org.noear.water.dso.ConfigHandler;
-import org.noear.water.model.ConfigSetM;
+import lombok.extern.slf4j.Slf4j;
+import org.noear.solon.Utils;
+import org.noear.solon.cloud.CloudConfigHandler;
+import org.noear.solon.cloud.annotation.CloudConfig;
+import org.noear.solon.cloud.model.Config;
 
 /**
  * 配置订阅（获取配置的时实刷新）
  *
  * @author noear 2021/1/6 created
  */
-@WaterConfig("water")
-public class TestConfigHandler implements ConfigHandler {
+@Slf4j
+@CloudConfig("water")
+public class TestConfigHandler implements CloudConfigHandler {
     @Override
-    public void handler(ConfigSetM cfgSet) {
-
+    public void handler(Config config) {
+        if (Utils.isNotEmpty(config.value())) {
+            log.warn(config.value());
+        }
     }
 }
--- demo02-solon/demo02-app/src/main/java/demo/controller/TestMessageHandler.java
+++ demo02-solon/demo02-app/src/main/java/demo/controller/TestMessageHandler.java
@@ -1,22 +1,26 @@
 package demo.controller;
 
-import org.noear.solon.Utils;
-import org.noear.water.WaterClient;
-import org.noear.water.annotation.WaterMessage;
-import org.noear.water.dso.MessageHandler;
-import org.noear.water.model.MessageM;
+import org.noear.solon.cloud.CloudClient;
+import org.noear.solon.cloud.CloudEventHandler;
+import org.noear.solon.cloud.annotation.CloudEvent;
+import org.noear.solon.cloud.model.Event;
 import org.noear.water.utils.Datetime;
 
 /**
  * 消息订阅（可以自己发出去，再自己订阅回来；起来拆解性能消耗的作用）
  *
  * */
-@WaterMessage("test.hello")
-public class TestMessageHandler implements MessageHandler {
+@CloudEvent("test.hello")
+public class TestMessageHandler implements CloudEventHandler {
+
     @Override
-    public boolean handler(MessageM msg) throws Throwable {
+    public boolean handler(Event event) throws Throwable {
         Datetime dt = Datetime.Now().addSecond(10);
-        WaterClient.Message.sendMessage(Utils.guid(),"test.hello", msg.message, dt.getFulltime());
+
+        CloudClient.event()
+                .publish(new Event("test.hello", event.content()).scheduled(dt.getFulltime()));
+
         return true;
     }
 }
+
