ccc8ac118a8eea7feba249ce8ced1c5655962e14
noear <noear@live.cn>
2021-04-06 14:45:14 +0800

    1.3.19

---
+++ demo01-springboot/demo01-app/src/test/java/demo/ClientTest.java
@@ -0,0 +1,21 @@
+package demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.noear.solon.test.HttpTestBase;
+import org.noear.solon.test.SolonJUnit4ClassRunner;
+import org.noear.solon.test.SolonTest;
+
+/**
+ * @author noear 2021/4/6 created
+ */
+@RunWith(SolonJUnit4ClassRunner.class)
+@SolonTest(ClientApp.class)
+public class ClientTest extends HttpTestBase {
+    @Test
+    public void test() throws Exception {
+        String rst = path("/test").data("msg", "hello").post();
+
+        assert rst != null && rst.contains(":");
+    }
+}
