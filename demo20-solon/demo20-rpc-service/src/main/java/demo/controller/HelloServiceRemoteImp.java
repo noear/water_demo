84d07332c6ef7b905e375008cec64e32b626c89b
noear <noear@live.cn>
2021-05-27 10:44:56 +0800

    water:2.1.2 + solon:1.4.5

--- demo20-solon/demo20-rpc-service/src/main/java/demo/controller/HelloServiceRemoteImp.java
+++ demo20-solon/demo20-rpc-service/src/main/java/demo/controller/HelloServiceRemoteImp.java
@@ -3,13 +3,14 @@ package demo.controller;
 import demo.protocol.HelloService;
 import org.noear.solon.annotation.Component;
 import org.noear.solon.annotation.Mapping;
+import org.noear.solon.annotation.Remoting;
 import org.noear.solon.core.handle.Context;
 
 /**
  * @author noear 2021/1/8 created
  */
 @Mapping("/rpc/")
-@Component(remoting = true)
+@Remoting
 public class HelloServiceRemoteImp implements HelloService {
     @Override
     public String hello() {
